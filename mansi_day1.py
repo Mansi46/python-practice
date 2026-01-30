dc38508574d997a476993a59700501383c3ff743
Mansi46 <mansi46577@gmail.com>
2026-01-30 13:07:17 +0530

    Update mansi_day1.py

--- mansi_day1.py
+++ mansi_day1.py
@@ -1,2 +1,14 @@
 print("Hello GitHub")
 print("My first Python program")
+
+#---------------
+#Variables
+name="Mansi"
+age=19
+is_student=True
+height=5.2
+
+print(name)
+print(age)
+print(is_student)
+print(height)
