ee63ae9c604d679878f459081156207541dbe471
Mansi46 <mansi46577@gmail.com>
2026-01-30 13:25:54 +0530

    Update mansi_day1.py

--- mansi_day1.py
+++ mansi_day1.py
@@ -12,3 +12,26 @@ print(name)
 print(age)
 print(is_student)
 print(height)
+
+#-----------------
+#Data Types
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(height))
+
+#----------------
+#Operators
+a=10
+b=3
+
+print("Addition", a+b)
+print("Subtract", a-b)
+print("Multiplication", a*b)
+print("Divide", a/b)
+print("Modulus", a%b)
+
+#------------------------
+#User Input (basic)
+user_name=input("Enter your name:")
+print(Welcome", user_name)
